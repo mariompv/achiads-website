20769500e20748f1a53c3773103d17857a3771b9
Mario Muñoz <mario.munozvillalobos@gmail.com>
2025-06-11 16:56:24 -0400

    add image upload-firebase

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyA8cKi8j4S0xtPhQne1N22_DQ78mFX_9lE",
     authDomain: "web-page-achiad.firebaseapp.com",
     projectId: "web-page-achiad",
-    storageBucket: "web-page-achiad.firebasestorage.app",
+    storageBucket: "web-page-achiad.appspot.com",
     messagingSenderId: "976716067507",
     appId: "1:976716067507:web:da643f2b8fa027904636c6",
     measurementId: "G-V6PBBB8WM9"
@@ -17,7 +17,9 @@ if (!firebase.apps.length) {
 // Exportar las instancias
 const db = firebase.firestore();
 const auth = firebase.auth();
+const storage = firebase.storage();
 
 // Hacer las variables disponibles globalmente
 window.db = db;
-window.auth = auth; 
+window.auth = auth;
+window.storage = storage; 
